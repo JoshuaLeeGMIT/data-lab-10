eced43808f009b9ff76ba97109f59f50c71140da
Joshua Lee <lee.son.wai@gmail.com>
2020-10-15 18:49:48 +0100

    app: Convert to a class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, world!</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, world!</h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
