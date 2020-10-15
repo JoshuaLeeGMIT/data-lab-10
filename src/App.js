73b2dad34ec04c660627665ed1a863a943011570
Joshua Lee <lee.son.wai@gmail.com>
2020-10-15 19:05:16 +0100

    components: Add a header component
    Also, make use of it in the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
+        <Header></Header>
         <header className="App-header">
           <h1>Hello, world!</h1>
           <h3>{new Date().toLocaleTimeString()}</h3>
