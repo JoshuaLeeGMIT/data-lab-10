9e8578849b17513bfe8a503c1524ab30a362a8bc
Joshua Lee <lee.son.wai@gmail.com>
2020-10-17 00:39:40 +0100

    app: Add simple routing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
 
 /* Named imports. */
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Header } from './components/header';
 import { Content } from './components/content';
 import { Footer } from './components/footer';
@@ -23,19 +24,23 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div className="App">
-          <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="/">Lab 2</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/read">Read</Nav.Link>
-              <Nav.Link href="/create">Create</Nav.Link>
-            </Nav>
-          </Navbar>
-          <Header></Header>
-          <Content></Content>
-          <Footer></Footer>
-        </div>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/">Lab 2</Navbar.Brand>
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/read">Read</Nav.Link>
+                <Nav.Link href="/create">Create</Nav.Link>
+              </Nav>
+            </Navbar>
+            <Switch>
+              <Route exact path="/" component={Header}/>
+              <Route exact path="/read" component={Content}/>
+              <Route exact path="/create" component={Footer}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </>
     )
   }
