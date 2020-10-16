097a9570898ad7d285558dc72aef161af389a97d
Joshua Lee <lee.son.wai@gmail.com>
2020-10-17 00:24:32 +0100

    app: Introduce a navbar
    Also take the opportunity to reorganise the imports.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+/* react-boostrap recommends importing individual components instead of the
+ * entire library. See:
+ *
+ * https://react-bootstrap.netlify.app/getting-started/introduction/
+ */
+
+ /* Default imports. */
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
-import './App.css';
+
+/* Named imports. */
+import { Header } from './components/header';
 import { Content } from './components/content';
 import { Footer } from './components/footer';
-import { Header } from './components/header';
+
+/* CSS imports. */
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
-      </div>
+      <>
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="/">Lab 2</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+          <Header></Header>
+          <Content></Content>
+          <Footer></Footer>
+        </div>
+      </>
     )
   }
 }
