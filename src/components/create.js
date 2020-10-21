61e090e6109dc3a0be378353d76d9f23963a8413
Joshua Lee <lee.son.wai@gmail.com>
2020-10-21 20:37:16 +0100

    components: Add more

---
+++ src/components/create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export class Create extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Hello from the create component</h3>
+      </div>
+    )
+  }
+}
---
+++ src/components/read.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export class Read extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Hello from the read component</h3>
+      </div>
+    )
+  }
+}
