edb55beacd33c2f805c934b6bcc5660eb4528a98
Joshua Lee <lee.son.wai@gmail.com>
2020-10-23 00:04:01 +0100

    components: Add new movieItem component

---
+++ src/components/movieItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+export class MovieItem extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
