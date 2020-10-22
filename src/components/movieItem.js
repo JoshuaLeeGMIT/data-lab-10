217fddc5f7037d80fbf606b63877120feaa91347
Joshua Lee <lee.son.wai@gmail.com>
2020-10-23 00:51:44 +0100

    components: Hook up movie display

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -6,6 +6,9 @@ export class MovieItem extends React.Component {
   render() {
     return (
       <div className="App">
+        <h4>{this.props.movie.Title}</h4>
+        <h5>{this.props.movie.Year}</h5>
+        <img src={this.props.movie.Poster} height="256"/>
       </div>
     )
   }
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { MovieItem } from './movieItem';
 import '../App.css';
 
 export class Movies extends React.Component {
 
   render() {
-    return (
-      <div className="App">
-      </div>
-    )
+    return this.props.movies.map((movie) => {
+      return <MovieItem movie={movie}></MovieItem>
+    })
   }
 }
