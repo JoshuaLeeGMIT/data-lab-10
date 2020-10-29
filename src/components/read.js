55766ede3a9768616e72d252a51e3ec62b0013bc
Joshua Lee <lee.son.wai@gmail.com>
2020-10-29 21:56:22 +0000

    read: Retrieve movie data from the web

--- src/components/read.js
+++ src/components/read.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import { Movies } from './movies';
 import '../App.css';
@@ -6,6 +7,20 @@ export class Read extends React.Component {
   /* Object's state. */
   state = {
     movies: []
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032")
+      .then(
+        (response) => {
+          this.setState({movies: response.data.Search})
+        }
+      )
+      .catch(
+        (e) => {
+          console.log(e);
+        }
+      );
   }
 
   render() {
